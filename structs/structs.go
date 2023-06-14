9fdea6f770165a8c3f827c4338081c37417d9f30
anjali29singh <anjalisingh930260@gmail.com>
2023-06-14 13:13:10 +0530

    interfaces

--- structs/structs.go
+++ structs/structs.go
@@ -6,22 +6,6 @@ import (
 
 // In golang in structs there is no concept of inheritance, parent, child
 
-//function in structs
-
-func main() {
-
-	anjali := User{"anjali", "anjali@gmail.com", 20, true,
-		Address{
-			"Bhopal", "MP", "Wallstreet", 34,
-		},
-	}
-
-	fmt.Println(anjali)
-	fmt.Printf("detail of anjali is %+v \n", anjali) //+v when structs are used
-	fmt.Printf("detail info of anjali is %+v \n", anjali)
-
-}
-
 type Address struct {
 	City    string
 	State   string
@@ -34,4 +18,31 @@ type User struct {
 	Age     int
 	Status  bool
 	Address Address
+}
+
+// methods
+// getAge() method will act on u of type User
+func (u User) getAge() int {
+	return u.Age
+
+}
+
+func (u *User) setAge(newage int) {
+	u.Age = newage
+}
+func main() {
+
+	anjali := User{"anjali", "anjali@gmail.com", 20, true,
+		Address{
+			"Bhopal", "MP", "Wallstreet", 34,
+		},
+	}
+
+	fmt.Println(anjali)
+	fmt.Printf("detail of anjali is %+v \n", anjali) //+v when structs are used
+	fmt.Printf("detail info of anjali is %+v \n", anjali)
+	fmt.Println(anjali.getAge())
+
+	anjali.setAge(21)
+	fmt.Println(anjali.getAge())
 }
